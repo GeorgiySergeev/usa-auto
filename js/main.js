f5fe5da5743dcfabd83e2cfcefa1c289fd9cf7bb
George <126503204+GeorgiySergeev@users.noreply.github.com>
2025-02-23 23:38:55 +0200

    upload

---
+++ js/main.js
@@ -0,0 +1,31 @@
+import '../scss/style.scss';
+
+// const header = document.querySelector('.header');
+// const main = document.querySelector('.main');
+// const footer = document.querySelector('.footer');
+
+// const headerHeight = header.offsetHeight;
+
+// main.style.paddingTop = `${headerHeight}px`;
+
+// const nav = document.querySelector('.nav');
+// const navList = document.querySelector('.nav__list');
+// const navItems = document.querySelectorAll('.nav__item');
+// const navLinks = document.querySelectorAll('.nav__link');
+
+// navList.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     const target = e.target;
+//     const link = target.closest('.nav__link');
+//     if (!link) return;
+    
+// });
+
+
+
+
+
+
+
+
+
